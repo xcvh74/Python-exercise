d88d8dc0768bf2f17a3ff019c88461e4498a9acc
xcvh74 <81223390+xcvh74@users.noreply.github.com>
2021-03-24 17:55:17 +0800

    Add files via upload

---
+++ 23張菀菁Exercise_3.py
@@ -0,0 +1,12 @@
+Num=eval(input("輸入一個數字"))
+if (Num%40)<=0:
+    print(Num,"是5也是8的倍數")
+elif (Num%5)<=0: 
+    print(Num,"是5的倍數")
+elif (Num%8)<=0:
+    print(Num,"是8的倍數")
+else:
+    print(Num,"不是5也不是8的倍數")
+
+
+    
