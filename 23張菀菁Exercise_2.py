6429a49f4b9cb9f8e60bced0e5faa7157d6d79ea
xcvh74 <81223390+xcvh74@users.noreply.github.com>
2021-03-24 16:16:11 +0800

    Add files via upload

---
+++ 23張菀菁Exercise_2.py
@@ -0,0 +1,12 @@
+def compute():    
+    NumA=int(eval(input("請輸入第一個整數")))
+    NumB=int(eval(input("請輸入第二個整數")))
+    print("數字",NumA,"的",NumB,"次方為",pow(NumA,NumB))
+compute()
+
+#Update
+NumA=int(eval(input("請輸入第一個整數")))
+NumB=int(eval(input("請輸入第二個整數")))
+def compute():    
+    print("數字",NumA,"的",NumB,"次方為",pow(NumA,NumB))
+compute()
