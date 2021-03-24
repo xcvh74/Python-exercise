84ec0cdc43673e2cf8221b6eb23e18def9c5d3cb
xcvh74 <81223390+xcvh74@users.noreply.github.com>
2021-03-25 00:40:54 +0800

    Update 23張菀菁Exercise_3.py

--- 23張菀菁Exercise_3.py
+++ 23張菀菁Exercise_3.py
@@ -1,12 +1,9 @@
-Num=eval(input("輸入一個數字"))
-if (Num%40)<=0:
+Num=eval(input("輸入一個數字:\t"))
+if (Num%5==0 and Num%8==0): 
     print(Num,"是5也是8的倍數")
-elif (Num%5)<=0: 
+elif (Num%5==0): 
     print(Num,"是5的倍數")
-elif (Num%8)<=0:
+elif (Num%8==0):
     print(Num,"是8的倍數")
 else:
-    print(Num,"不是5也不是8的倍數")
-
-
-    
+    print(Num,"不是5也不是8的倍數")   
